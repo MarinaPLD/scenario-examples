fdca5122a2debf1e5bf5905bdc73b10b00dae9bc
Marina DIMITROVA <marina-dimitrova@hotmail.fr>
2024-12-11 12:07:36 +0100

    Latest

--- network-traffic/background.sh
+++ network-traffic/background.sh
@@ -6,11 +6,8 @@ iptables -t nat -A PREROUTING -p tcp --dport 3:65535 -j REDIRECT --to-port 2
 # We need to change the WEB_UI_HOST variable in the docker-compose.yml file to a valid generated host
 sed -i 's|${WEB_UI_HOST}|'"$(sed 's/PORT/1/g' /etc/killercoda/host | cut -d '/' -f 3)"'|g' docker-compose.yml
 
-echo "Updating docker ..."
-rm -f /usr/bin/docker
-curl -fsSL https://get.docker.com/ | sh
-
-echo "Starting docker daemon ..."
-dockerd &>/dev/null &
-
-echo "done" > /tmp/background
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+COMPOSE_VERSION="$(curl -i https://github.com/docker/compose/releases/latest | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+')"
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/"$COMPOSE_VERSION"/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
--- network-traffic/foreground.sh
+++
@@ -1,3 +0,0 @@
-echo "waiting for init-background-script to finish"
-while [ ! -f /tmp/background ]; do sleep 1; done
-echo "Here we go! 🚀"
