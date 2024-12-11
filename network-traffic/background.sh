168ffe3306d716932786bdafa10aa86d11e40593
Marina DIMITROVA <marina-dimitrova@hotmail.fr>
2024-12-11 12:14:59 +0100

    Latest

--- network-traffic/background.sh
+++ network-traffic/background.sh
@@ -1,5 +1,3 @@
-set -x
-
 # We need to redirect all traffic from port 3-65535 to port 2 so the user can create as many services as they want
 iptables -t nat -A PREROUTING -p tcp --dport 3:65535 -j REDIRECT --to-port 2
 
@@ -9,5 +7,5 @@ sed -i 's|${WEB_UI_HOST}|'"$(sed 's/PORT/1/g' /etc/killercoda/host | cut -d '/' 
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 COMPOSE_VERSION="$(curl -i https://github.com/docker/compose/releases/latest | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+')"
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/"$COMPOSE_VERSION"/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
